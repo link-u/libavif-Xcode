b1c8df86469288590ca6519a09115d23a8f5dcb2
y-hashida <yoshimasa.hashida@link-u.co.jp>
2024-01-18 22:49:27 +0900

    libdav1d-static の URL を ssh → https にした

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "git@github.com:link-u/libdav1d-static", from: "0.0.9")
+        .package(url: "https://github.com/link-u/libdav1d-static.git", from: "0.0.9")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
